4c30d71942f1011452181e2a2004d9e7136f552d
moliveros <mcoliveros1202@gmail.com>
2022-06-08 18:03:41 -0500

    set up searches saved to localStorage

--- assets/js/take2.js
+++ assets/js/take2.js
@@ -6,26 +6,20 @@ var formEl = document.querySelector("#city-form");
 
 var currentContainer = document.getElementById("today-container");
 var fiveDayContainer = document.getElementById("five-day");
-// var cityDisplay = document.querySelector();
-// var tempDisplay = document.querySelector();
-// var windDisplay = document.querySelector();
-// var humidityDisplay = document.querySelector();
-// var uvDisplay = document.querySelector();
 
 // submit city search
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
     // get value from input element
     var city = cityInputEl.value.trim();
-    
+
     if (city) {
         getCoord(city);
 
-        cityInputEl.value="";
+        cityInputEl.value = "";
     } else {
         alert("Please enter a city name");
     }
-
 }
 
 // convert city name to coordinates
@@ -43,17 +37,20 @@ var getCoord = function (cityName) {
     });
 };
 
-var convertCoord = function(city) {
+var convertCoord = function (city) {
+    // create variables for lat and lon to plug into coordApiURL
+
     var lat = city.coord.lat;
     var lon = city.coord.lon;
     console.log(city.name);
-    var coordApiURL =`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=2a7c7bd65f9deac59c01e0be3fce7c26&units=imperial`;
+    var coordApiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=2a7c7bd65f9deac59c01e0be3fce7c26&units=imperial`;
 
     // request to URL
     fetch(coordApiURL).then(function (response) {
         response.json().then(function (data) {
-            var date = new Date (data.current.dt * 1000);
-            
+            var date = new Date(data.current.dt * 1000);
+
+            // display current forecast
             currentContainer.innerHTML = `<h3 class="card-header">${city.name} ${date.toDateString()} <img 
             src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}.png" alt="${data.current.weather[0].description}"/> </h3>
 
@@ -65,10 +62,11 @@ var convertCoord = function(city) {
             <p>UV Index: ${data.current.uvi}</p>
             </div>`;
 
-        fiveDayContainer.innerHTML = data.daily.map((day,index) => {
-            if (index > 0 && index < 6) {
-                var dt = new Date (day.dt * 1000);
-                return `<div class="card">
+            // display 5-day forecast
+            fiveDayContainer.innerHTML = data.daily.map((day, index) => {
+                if (index > 0 && index < 6) {
+                    var dt = new Date(day.dt * 1000);
+                    return `<div class="card">
                 <div class="card-body">
                     <p class="card-text">
                     <h4>${dt.toDateString()}</h4>
@@ -79,16 +77,42 @@ var convertCoord = function(city) {
                     <p>Humidity: ${day.humidity}%</p>
                 </div>
             </div>`
-            }
-        }).join('');
+                }
+            }).join('');
+
+            searchHistory = coordApiURL
+            saveSearch();
         });
+        // save to localStorage
+        var saveSearch = function () {
+            localStorage.setItem("searchHistory", JSON.stringify(coordApiURL));
+        }
     });
+    // var displayHistory = function () {
+    //     var savedSearches = localStorage.getItem("coordApiURL");
+
+    //     // if no searches, set search history to an empty array and return out of function
+    //     if (!savedSearches) {
+    //         return false;
+    //     }
+    //     console.log("Saved searches found!");
+    //     // else load saved searches
+
+    //     // parse into array of object
+    //     savedSearches = JSON.parse(savedSearches);
+
+    //     // loop through savedSearches array
+    //     for (var i = 0; i < savedSearches.length; i++) {
+    //         convertCoord(savedSearches[i]);
+    //     }
+
+    // };
 };
 
+
+
 formEl.addEventListener("submit", formSubmitHandler);
-
-
-// display localStorage
+// displayHistory();
 
 
 // click history to go to city forecast
